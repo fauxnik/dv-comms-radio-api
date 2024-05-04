4c271125b617a4cfa8aedcb26b2dad1927cf4439
Niko Fox <niko@fauxnik.me>
2024-05-03 17:21:38 -0700

    Document vanilla sound asset names
    Thanks cruzerblade9369!

--- CommsRadioAPI/VanillaSoundCommsRadio.cs
+++ CommsRadioAPI/VanillaSoundCommsRadio.cs
@@ -5,11 +5,30 @@ namespace CommsRadioAPI;
 /// </summary>
 public enum VanillaSoundCommsRadio
 {
+	/// <summary>
+	/// CommsRadio_Confirm_01
+	/// </summary>
 	Confirm,
+	/// <summary>
+	/// CommsRadio_Cancel_01
+	/// </summary>
 	Cancel,
+	/// <summary>
+	/// CommsRadio_Message_01
+	/// </summary>
 	Warning,
+	/// <summary>
+	/// CommsRadio_Confirm_01<br/>
+	/// This may play CommsRadio_Select_01 in the future. Use `Confirm` instead for CommsRadio_Confirm_01.
+	/// </summary>
 	Switch,
+	/// <summary>
+	/// CommsRadio_Apply_01
+	/// </summary>
 	ModeEnter,
+	/// <summary>
+	/// CommsRadio_Hover_01
+	/// </summary>
 	HoverOver,
 	MoneyRemoved,
 }
